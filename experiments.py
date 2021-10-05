c70cd40fec56abf79e269cb0fc42fab42f70dee7
fragnarxx <fabio25.rodriguez@gmail.com>
2021-10-05 11:28:14 +0200

    Muestra de experimento para AnaV

--- experiments.py
+++ experiments.py
@@ -45,8 +45,77 @@ def simulate(timestep, drone_parameters, drone_positions, goals):
 
 
 
+
+
 if __name__ == "__main__":
 
-    timestep, drone_parameters, drone_positions, goals = experiment1()
+    # timestep, drone_parameters, drone_positions, goals = experiment1()
 
-    return simulate(timestep, drone_parameters, drone_positions, goals)
+    # return simulate(timestep, drone_parameters, drone_positions, goals)
+
+    i = 0
+    N = 1000
+    while i < N:
+
+        posiciones = []
+        for i in range(6):
+            x = random.uniform(-25, 25)
+            y = random.uniform(-25, 25)
+            posiciones.append((x, y))
+
+        goals = []
+        for i in range(6):
+            x = random.uniform(-25, 25)
+            y = random.uniform(-25, 25)
+            goals.append((x, y))
+
+        distanciadeseguridad = 3
+        auxiliar = False
+        for i in range(len(posiciones)):    
+            for j in range(i+1, len(posiciones)):
+                pos1 = posiciones[i]
+                pos2 = posiciones[j]
+                if dist(pos1, pos2) < distanciadeseguridad:
+                    auxiliar = True
+                    break 
+        
+        for i in range(len(goals)):    
+            for j in range(i+1, len(goals)):
+                pos1 = goals[i]
+                pos2 = goals[j]
+                if dist(pos1, pos2) < distanciadeseguridad:
+                    auxiliar = True
+                    break 
+
+        if auxiliar:
+            continue
+
+        ### Imprimir experimento en txt (posiciones y goals)
+
+        ## Haces el experimento
+
+        ### Imprimir resultados en txt (diferentes medidas)
+
+        i+=1
+
+        ## i.TXT
+        # pocisiones = [(x1, y1), ... ,(xn, yn)]
+        # goals = [(x1, y1), ... ,(xn, yn)]
+        # tiempo: MEDIDA DE TIEMPO
+        # trayectoria: ...
+        # ....
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
